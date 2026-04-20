eccdcb9d073e252645d3dd12eb1d17a006b0b75b
Saket Tommundrum <hs3322@wayne.edu>
2026-04-20 11:32:35 -0400

    login and logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -62,3 +62,38 @@ export const signup = async (req,res) => {
     }
 }
 
+export const login = async(req,res) =>{
+    const {email, password}=req.body
+
+    try{
+        const user=await User.findOne({email})
+        if (!user) return res.status(400).json({message:'Invalid Credentials'})
+        
+        const isPasswordCorrect =await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) return res.status(400).json({message:'Invalid Credentials'})
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+        console.log("Logged In succesfully")
+
+    } catch(error){
+
+        console.error("Error in Login controller:", error);
+        res.status(500).json({message: "Internal server error"});
+
+    }
+
+};
+
+export const logout = (_,res) =>{
+    res.cookie("jwt", "", {maxAge:0})
+    res.status(200).json({message:"Logout successful"})
+    
+};
+
